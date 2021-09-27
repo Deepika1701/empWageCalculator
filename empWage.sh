2678b616529ec56c2df8f96b96ac7dcbab86600b
Deepika1701 <deepikakumari1701.com>
2021-09-27 21:41:47 +0530

    Added: code for calculating daily wage

--- empWage.sh
+++ empWage.sh
@@ -1,10 +1,15 @@
 echo "Welcome to employee wage computation:-"
 isPresent=0
 isAbsent=1
+salaryPerHr=20
 randomNumber=$((RANDOM%2))
 if [ $randomNumber -eq isPresent ]
 then
+   empHr=8
    echo "Employee is present"
 else
+   empHr=0
    echo "Employee is absent"
 fi
+salary=$((salaryPerHr*empHr))
+echo $salary
