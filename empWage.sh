fa393af05828090b4b2bc2f319546117af48df49
Deepika1701 <deepikakumari1701.com>
2021-09-29 23:23:46 +0530

    Added: code for calculating wages till a condition of total working hours or day is reached for a month

--- empWage.sh
+++ empWage.sh
@@ -3,19 +3,39 @@ isPresentFullTime=0
 isPresentPartTime=1
 isAbsent=2
 salaryPerHr=20
-for((day=1;day<=20;day++))
+totalHr=0
+totalworkingdays=0
+day=1
+while [ $day -le 30 ]
 do
    randomNumber=$((RANDOM%3))
    if [[ $randomNumber -eq $isPresent ]]
    then
         empHr=8
+   if (( ($totalHr+$empHr)>100 || $totalworkingdays>20 ))
+   then
+      break
+
+   fi
+        totalHr=$((totalHr+empHr))
+        ((totalworkingdays++))
+
    elif [ $randomNumber -eq $isPresentPartTime ]
    then
         empHr=4
+   if (( ($totalHr+$empHr)>100 || $totalworkingdays>20 ))
+   then
+      break
+   fi
+
+        totalHr=$((totalHr+empHr))
+        ((totalworkingdays++))
    else
         empHr=0
    fi
    totalsalary=$((totalsalary+(salaryPerHr*empHr)))
+   ((day++))
 done
 echo "total salary "$totalsalary
-
+echo "total working hours "$totalHr
+echo "total days of work " $totalworkingdays
